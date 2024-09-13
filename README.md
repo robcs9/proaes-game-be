761df9f0f00bb72e02d482df16500fa74f47ab13
robcs9 <83130807+robcs9@users.noreply.github.com>
2024-09-13 09:23:06 -0300

    Atualizando algumas definições de testes iniciais de ferramentas

--- README.md
+++ README.md
@@ -9,6 +9,13 @@ Parâmetros:
 - Mobiliado: Sim
 - Localidades: Cidade Universitária, Várzea, Engenho do Meio, Jardim São Paulo, Iputinga, Torrões, Cordeiro, Madalena, Torre
 
+### Estudo:
+
+- [Scraping](https://www.scrapehero.com/web-scraping-with-pandas/)
+- [Selenium](https://selenium-python.readthedocs.io/getting-started.html)
+- [LXML / BeautifulSoup] (Pending)
+
+
 ### TO-DO
 
-- [x] (Pending...)
+- [x] Cruzar informações para tentar encontrar as plataformas/canais de anúncios utilizadas pelos estabelecimentos encontrados no Google Maps
--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import pandas as pd
 import requests as rq
 from io import StringIO
 
+# Pensões
+# https://www.google.com/search?sca_esv=ea85460ec5208a83&tbs=lf:1,lf_ui:2&tbm=lcl&q=moradias+estudantis+recife&rflfq=1&num=10&sa=X&ved=2ahUKEwik5rzu5L-IAxWMq5UCHSdXDRkQjGp6BAgqEAE&biw=1858&bih=972#rlfi=hd:;si:;mv:[[-8.015099137973076,-34.911359265795404],[-8.085044181233394,-34.97736538232835]]
+url_webq1 = "https://www.webquarto.com.br/busca/pensionatos/recife-pe?price_range[]=0,15000&has_photo=0&smokers_allowed=0&children_allowed=0&pets_allowed=0&drinks_allowed=0&visitors_allowed=0&couples_allowed=0"
+
+# Repúblicas
+# https://www.google.com/search?q=rep%C3%BAblica+estudantil+recife&sca_esv=ea85460ec5208a83&biw=896&bih=971&tbm=lcl&ei=_yfkZpydAqy_1sQPlrKl6A8&oq=republiestudantis+recife&gs_lp=Eg1nd3Mtd2l6LWxvY2FsIhhyZXB1YmxpZXN0dWRhbnRpcyByZWNpZmUqAggAMgcQABiABBgNMggQABgHGAgYHjIIEAAYgAQYogQyCBAAGIAEGKIEMggQABiABBiiBEibYlDdQVjNTnABeACQAQCYAb8BoAH-DKoBBDAuMTC4AQPIAQD4AQGYAgegApAIwgIIEAAYFhgeGA_CAgYQABgeGA_CAggQABgHGB4YD8ICBhAAGAgYHpgDAIgGAZIHAzEuNqAHoDg&sclient=gws-wiz-local#rlfi=hd:;si:;mv:[[-8.033457073050673,-34.92552083401562],[-8.068428905050208,-34.95852331247459]]
+url_webq2 = "https://www.webquarto.com.br/busca/republicas/recife-pe?price_range[]=0,15000&has_photo=0&smokers_allowed=0&children_allowed=0&pets_allowed=0&drinks_allowed=0&visitors_allowed=0&couples_allowed=0"
+
+# Apto
 url_zap = "https://zapimoveis.com.br/aluguel"
 url_zap_filtro = "https://www.zapimoveis.com.br/aluguel/apartamentos/pe+recife++cid-universitaria/\
             mobiliado/?__ab=sup-hl-pl:newC,exp-aa-test:B,super-high:new,olx:control,\
@@ -22,10 +31,23 @@ url_zap_filtro = "https://www.zapimoveis.com.br/aluguel/apartamentos/pe+recife++
             ,condominio_residencial,flat_residencial,loft_residencial&pagina=1&\
             amenities=Mobiliado&ordem=Menor%20pre%C3%A7o"
 url_olx = "https://www.olx.com.br/estado-pe?q=aluguel&cg=1000"
-headers = {"User-Agent": "Mozilla/5.0"}
 
-response = rq.get("https://quintoandar.com.br", headers=headers)
+# "Quartos"
+# https://www.olx.com.br/imoveis/aluguel/estado-pe?q=quartos%20para%20estudantes&sp=1
+# https://www.olx.com.br/imoveis/estado-pe?q=aluguel%20quarto&sp=1
+headers = {
+    "User-Agent": "Mozilla/5.0",
+    "X-Requested-With": "XMLHttpRequest"
+}
+
+# requests
+url_test = "https://www.amazon.com/Apple-iPhone-Fully-Unlocked-128/dp/B07P611Q4N"
+response = rq.get(url_test, headers=headers)
 #print(response._content)
-data = pd.read_html(response._content)
-print(pd.read_html("http://google.com"))
 
+# pandas - for tabular data handling
+#data = pd.read_html(response.text)
+#print(pd.read_html("http://google.com"))
+print(pd.read_html(response.text))
+
+# LXML / BeautifulSoup - non-tabular data scraping
