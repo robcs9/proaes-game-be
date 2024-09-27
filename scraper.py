ae66bb06bcbb41692f0cfb96fbb215bfd0f3e762
robcs9 <83130807+robcs9@users.noreply.github.com>
2024-09-27 10:57:05 -0300

    Atualização do scraper

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 # import numpy as np
 # import matplotlib.pyplot as plt
 # import seaborn as sns
+import time
 import requests as rq
 import pandas as pd
 from urllib.request import urlopen
@@ -9,9 +10,43 @@ from io import StringIO
 import json
 
 headers = {
-    "User-Agent": "Mozilla/5.0",
-    "X-Requested-With": "XMLHttpRequest"
+    #"User-Agent": "Mozilla/5.0",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0",
+    "X-Requested-With": "XMLHttpRequest",
+    #"Referer": "https://www.olx.com.br/",
 }
+
+# OLX req headers
+h = {
+		"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/jxl,image/webp,image/png,image/svg+xml,*/*;q=0.8",
+		"Accept-Encoding": "gzip, deflate, br, zstd",
+		"Accept-Language": "en-US,en;q=0.5"
+		,
+		"Connection": "keep-alive",
+		"Cookie": "r_id=450519f9-ec6d-4738-8962-38377a678a7c; nl_id=2e281b9d-c8c0-4176-8bbe-caeb41724dab; cf_clearance=jX12rt7F.GFQE56N5zVsQK1D88fPEHcxB0znvr8yaGM-1726229270-1.2.1.1-C2Bu2_tnzT4SFiBtGxLH.KlYgOeSdvbrBMaDhRaY.eV.uOb9_55KFQHOT.iJiyMGZ0KZogjHc5LJT4IhMB88eI2LVBAu_Yv4z5tFJA2Mw6J2S6xCDRu2ntZaDxyzxO.AJsOmd.XXZBL8W2qi7puKg_jsvrbXbe4dw4Wdh1CjspG0rsxNmhjhkO30QYbwjlsDTTTuj_liwkHZVYkhx_PyQVEDPuqGP36RHUmF3dNUONcOUL3DSr4aEwHYpoBIPnCPBJjWNAXRSBZdKxfE.gIFzc8XcRdp2vqTYwtjRhV.JFEYSsAYjiNqOqq_42aJAc_qFJqZwBIbisZQiQAdmXJwTjLL6Ti_fmiMo2qGuLENI6k; _cfuvid=atcWDdPAVKVhp9fN.e0jAZ2qVK640lcD849WlciTszU-1727434007433-0.0.1.1-604800000; TestAB_Groups=txpBdgOrd_entrega.ctafree-ai_on.ppc-myplan-redirect-lp_enabled.ds-header-navbar_enabled.rp-img_enabled.aps-autos-filters-fair_enabled.cmod-security-central-my-ads_control.cnt-rating_v3.trp-cvv-ck_enabled.bjTPZ-aps_enabled.payg-discount-re-julius_ml-ranges.sanityweb50_control.ds-adcard-optimized_on.cnt-scchat_control.prf-authV2_enabled.sxpswgoods_enabled.con-ssr_control.ck-filters_enabled.nv2-modal_enabled.txpNuPay_enabled.adv-li60f8_enabled.autospp-notshow-modal-hv-myads_enabled.autospp-marketplace-header_enabled.payg-discount-julius_ml-c-mab.menu-v1_vrtcal.acrc-reg_control.ck-2-cat_enabled.posfricars_enabled.rp-dup-lnk_enabled.acrc-myacc_control.periodic_12m.fe-bst-gll_control.auto-steps_enabled.must-optin_optin.txtbanrswt_enabled.card-mrp_control.aps-autos-fair-banner_enabled.aps-autos-fair-badge_enabled.nv1-rating_control.auto-placa_enabled.ck-ai-req_enabled.ppf-sub-nc_enabled.sxp-adopt_enabled.adcard-vid_control.acc-phones_enabled.app-bprice_enabled.pass-lvl_enabled.ppffere_control.posautncv2_enabled.ppffernwre_enabled.adv-lifbd3_before.con-optin_A.Autos-foto_enabled.trp-comp_control.poseditrec_diamond.ppfncvch_control.ppffrrnwre_enabled.adv-ho5f03_enabled.crsl-del_control.app-loc_control.swtautnc_enabled.ck-refpric_enabled.semcaptcha_enabled.adv-ad61c2_enabled.fr-shp-ai_enabled.pric-cents_enabled.txpAddCard_control.ai-img-up_control.ck-fipe_enabled.fernwcars_control.hot-ad_enabled.fernwautnc_control; __cf_bm=6hxxcKYrQLnKDFe2e4JYbfxgIKoMXNlWkt_.7srEZ3A-1727434007-1.0.1.1-MlkFRH3qPeiRaupFARLtoyaAIBLD23HomreRjvT6Cekihq5GIkrvygHCiP6BXRQD7.z.dxQAbuYYoZb.mw7BdA",
+		"DNT": "1",
+		"Host": "www.olx.com.br",
+        "If-None-Match": "\"hdxbmgzh61ulzv\"",
+		"Priority": "u=0, i",
+        "Sec-Fetch-Dest": "document",
+		"Sec-Fetch-Mode": "navigate","Sec-Fetch-Site": "cross-site",
+        "Sec-GPC": "1",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0",
+}
+HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Cache-Control": "max-age=0",
+    }
 
 # url = "http://www.hubertiming.com/results/2017GPTR10K"
 # html = urlopen(url)
@@ -22,13 +57,36 @@ headers = {
     # print(f'link: ${link}')
 
 # OLX
-olx = "https://www.olx.com.br/imoveis/aluguel/estado-pe/grande-recife/recife?pe=2000&ret=1020&ret=1060&ret=1040&sd=3747&sd=3778&sd=3766&sd=3764&sd=3762"
+url_olx = "https://www.olx.com.br/imoveis/aluguel/estado-pe/grande-recife/recife?pe=2000&ret=1020&ret=1060&ret=1040&sd=3747&sd=3778&sd=3766&sd=3764&sd=3762"
 
 # WebQuartos
 url = "https://www.webquarto.com.br/busca/quartos/recife-pe/Cordeiro%7CV%C3%A1rzea%7CTorre%7CTorr%C3%B5es%7CMadalena%7CIputinga?price_range%5B%5D=0,2200&has_photo=0&smokers_allowed=0&children_allowed=0&pets_allowed=0&drinks_allowed=0&visitors_allowed=0&couples_allowed=0"
 
 #wq_r = rq.get(url, headers=headers)
 #soup1 = BeautifulSoup(wq_r.text, 'lxml')
+
+def search_OLX():
+    # class = olx-ad-card olx-ad-card--horizontal olx-ad-card--highlight
+    html = rq.get("https://olx.com.br", headers=h)
+    # time.sleep(5) # sleep for JS full loading
+    soup = BeautifulSoup(html._content, 'lxml')
+    sections = soup.find_all("section")
+    #section_search = soup.find_all("section", attrs={"data-ds-component": "DS-AdCard"})
+    
+    # filters don't work for now
+    #filtered = soup.find("section", class_="olx-ad-card")
+    
+    # html_test = '<div name="somediv">somediv</div><section class="another section">another<section id="1" class="olx-ad-card olx-ad-card--horizontal olx-ad-card--highlight" data-ds-component="DS-AdCard"></section><section class="one more section">more</section>'
+    # test = BeautifulSoup(html_test, 'lxml')
+    # test_result = test.find("section", class_="more")#.prettify()
+    
+    # strip().split('<section data-ds-component="DS-AdCard" class="olx-ad-card olx-ad-card--horizontal olx-ad-card--highlight">')
+    result = ""
+    for sect in sections:
+        print("\n------:", sect.find("a")) #class_=""))
+    #print(result)
+
+search_OLX()
 
 def search_WQ():
     html = urlopen(url)
@@ -52,4 +110,4 @@ def search_WQ():
     # Dados dos anúncios da busca no WebQuartos
     print(data_json["ads"])
     
-search_WQ()
+# search_WQ()
