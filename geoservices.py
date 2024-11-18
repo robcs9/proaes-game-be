b89fe3ee275852cb1ec855b69d3453081b3c307a
robcs9 <83130807+robcs9@users.noreply.github.com>
2024-11-18 11:41:39 -0300

    Atualizando arquivo de testes, e batchGeocode em geoservices

--- geoservices.py
+++ geoservices.py
@@ -1,5 +1,7 @@
 import os
+from time import sleep
 import requests
+import json
 # Loads config altering the env 
 # from dotenv import load_dotenv
 # load_dotenv()
@@ -8,6 +10,7 @@ import requests
 # Loads config without altering the environment
 from dotenv import dotenv_values
 config = dotenv_values('.env')
+GEOAPIFY_API_KEY = config['GEOAPIFY_API_KEY']
 
 def normalizeCep(cep: str):
     if len(cep) > 9 or len(cep) < 8:
@@ -18,7 +21,6 @@ def normalizeCep(cep: str):
 # It takes a well-formed CEP (12345-678) as argument
 def parseCoords(cep: str):
     cep = normalizeCep(cep)
-    GEOAPIFY_API_KEY = config['GEOAPIFY_API_KEY']
     api_url = f'https://api.geoapify.com/v1/geocode/search?postcode={cep}&country=brazil&type=postcode&format=json&apiKey={GEOAPIFY_API_KEY}'
     res = requests.get(api_url).json()['results']
     # return { 'lat': res[0]['lat'], 'lon': res[0]['lon'] } if len(res) > 0 else 'Erro: Coordenadas não encontradas - CEP inválido'
@@ -26,4 +28,49 @@ def parseCoords(cep: str):
     return f'{res[0]['lat']},{res[0]['lon']}' if len(res) > 0 else ''
 # print(parseCoords('54000-000'))
 
-# todo: Batch request for coords parsing
+# Batch request for geocoding of CEPs
+def batchGeocode(ceps: list):
+    url = f'https://api.geoapify.com/v1/batch?apiKey={GEOAPIFY_API_KEY}'
+    headers = {'Content-Type': 'application/json'}
+
+    inputs = []
+    for cep in ceps:
+        inputs.append({"params": {"postcode": cep}})
+    
+    data = {"api": "/v1/geocode/search","params": {"country": "brazil","type": "postcode", "format": "json"},"inputs":inputs}
+    data = json.dumps(data, ensure_ascii=False)
+    
+    try:
+        job_rq = requests.post(url, headers=headers, data=data).json()
+        job_url = job_rq['url']
+        
+        print(f'\nGeocoded CEP batch requested at {job_url}')
+        while True:
+            job = requests.get(job_url)
+            if job.status_code == 200:
+                job = job.json()
+                results = job['results']
+                print(f'\nBatch job done. Found {len(results)} results')
+                
+                coords = []
+                for result in results:
+                    r = result['result']['results']
+                    lat = ''
+                    lng = ''
+                    if len(r) > 0:
+                        lat = r[0]['lat']
+                        lng = r[0]['lon']
+                    coords.append({
+                        'cep': result['params']['postcode'],
+                        'lat': lat,
+                        'lng': lng
+                    })
+                return coords
+            print('\nBatch job ongoing...')
+            sleep(1)
+    except requests.exceptions.HTTPError as e:
+        print(e.response.text)
+
+    # use bias param to improve precision?
+
+# print(batchGeocode(['54330-075','54000-000','55000-000', '111-789']))
--- test.py
+++ test.py
@@ -7,4 +7,8 @@ class AddTests(unittest.TestCase):
         
 class CepToCoordsTests(unittest.TestCase):
     def test_coordinates_retrieval(self):
-        self.assertEqual(ctc.getCoordsFromCep('50810000'), {'lat': '1', 'lng': '2'})
+        # self.assertEqual(ctc.getCoordsFromCep('50810000'), {'lat': '1', 'lng': '2'})
+        pass
+    def test_batch_geocode(self):
+        res = ctc.batchGeocode(['54330-075','54000-000','55000-000'])
+        # aprint(res)
