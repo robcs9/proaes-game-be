a44d081280ce51fcfef21e7e3a84fcfdacbd66c8
robcs9 <83130807+robcs9@users.noreply.github.com>
2025-01-07 11:26:45 -0300

    Adicionando exception handler a  makeSoup

--- utils.py
+++ utils.py
@@ -11,10 +11,11 @@ import repository as repo
 url = "https://www.olx.com.br/imoveis/aluguel/estado-pe/grande-recife/recife?pe=2000&ret=1020&ret=1060&ret=1040&sd=3747&sd=3778&sd=3766&sd=3764&sd=3762&o=2"
 # headers = { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
 
-def search():
-    from curl_cffi import requests as currq
-    res = currq.get(url, impersonate="chrome")
-    soup = BeautifulSoup(res.text, 'lxml')
+def search(url):
+    # from curl_cffi import requests as currq
+    # res = currq.get(url, impersonate="chrome")
+    # soup = BeautifulSoup(res.text, 'lxml')
+    soup = makeSoup(url)
     print(soup.find('script', {"id": "__NEXT_DATA__"}).get_text())
 
 
@@ -88,8 +89,12 @@ def filterAds(new_ads: list[dict], prev_df):
 
 from curl_cffi import requests as curlrq
 def makeSoup(url: str):
-    content = curlrq.get(url, impersonate="chrome")
-    soup = BeautifulSoup(content.text, "lxml")
+    try:
+        content = curlrq.get(url, impersonate="chrome")
+        soup = BeautifulSoup(content.text, "lxml")
+    except Exception as e:
+        print(f"Erro durante scraping da url ({url}).\n{e}")
+        return
     return soup
 
 def parseAddress(cep: str):
@@ -134,6 +139,3 @@ def validateSavedData():
             repo.delete(idx)
         # else: make ad active again?
         print(f'\nValidated ads: {i+1}/{count}')
-
-#r = makeSoup("https://google.com/404")
-#print(r)
