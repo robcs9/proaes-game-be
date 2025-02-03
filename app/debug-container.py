2c67b5258f4b953e9cbd470dc8249b849711a9ff
robcs9 <83130807+robcs9@users.noreply.github.com>
2025-02-03 12:34:01 -0300

    Atualizando debug-container

--- app/debug-container.py
+++ app/debug-container.py
@@ -12,8 +12,12 @@ soup = BeautifulSoup(content.text, "lxml")
 # diagnose(bad_html)
 
 # print(soup.find('span'))
-chnk1 = soup.find('span', string=re.compile(', PE, 5'))
-chnk2 = chnk1.find_previous_sibling()
-print(chnk2, chnk1)
+print(f'span tags:\n{soup.find('span')}')
+address_tag2 = soup.find('span', string=re.compile(', PE, 5'))
+print(f'address:\n{address_tag2}')
+address_chunk1 = address_tag2.find_previous_sibling().getText(strip=True)
+address_chunk2 = address_tag2.getText(strip=True)
+address = f"{address_chunk1}, {address_chunk2}"
+# print(address)
 
 # lookup the tag script that has data-json attr if span tags lead nowhere
