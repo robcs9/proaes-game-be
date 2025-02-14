145998fa1231a63acfc00620aabaa786d0220b55
robcs9 <83130807+robcs9@users.noreply.github.com>
2025-02-14 08:54:39 -0300

    Atualizando deployment e docker

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
-# New CMD: crontab running the main script and copying 
-# the data.geojson to /usr/share/geojson/ right after
-
-
-# crontab -l
-# cat /etc/crontabs/root
-# ls /etc/cron.d
-# crond
-echo "Running scraper python script now..."
+echo "Executing python scraper script now..."
 python3 /code/main.py #&& cp /code/data/data.geojson /usr/share/geojson/
--- app/update_shared_data.sh
+++ app/update_shared_data.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-cp /code/data/data.geojson /usr/share/geojson/
+echo 'Copying data.geojson to shared volume'
+cp /code/data/data.geojson /usr/share/geojson/data.geojson
 echo 'The data.geojson file has been updated!'
