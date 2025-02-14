d05925e046f2ae49e94616dd15b9727ae7931e89
robcs9 <83130807+robcs9@users.noreply.github.com>
2025-02-14 09:05:28 -0300

    Atualizando overlapping coordinates handler

--- app/plot.py
+++ app/plot.py
@@ -32,6 +32,14 @@ def handleOverlapsCoords(df:pd.DataFrame=None, grp: DataFrameGroupBy=None):
     #   df.loc[idx, "lng"] = curr_lon
     #   # print(f'current ({curr_lat}, {curr_lon})')
     for idx in group.index:
+      if type(curr_lon) is not float and type(curr_lon) is not int:
+        print(
+          f"""Invalid longitude value during overlapping processing.
+          Lng value: {curr_lon}
+          Lng type: {type(curr_lon)}
+          """
+        )
+        continue
       distance = 0.001
       # curr_lat += 0.999
       curr_lon += distance
@@ -39,49 +47,3 @@ def handleOverlapsCoords(df:pd.DataFrame=None, grp: DataFrameGroupBy=None):
       df.loc[idx, "lng"] = curr_lon
       # print(f'current ({curr_lat}, {curr_lon})')
   return df
-
-def plotMap():
-  df = pd.read_csv('./data/data.csv', index_col=0)
-  df = scatterOverlaps(df)
-  center = dict(lat=-8.0513, lon=-34.9313)
-  fig = px.scatter_map(df, lat="lat", lon="lng", hover_name="title", hover_data=["title", "price", "url",],
-  color_discrete_sequence=["red"], zoom=13, height=700, center=center, opacity=0.2,
-  size=[15 for i in range(len(df))], )
-  
-  # fig.layout.title.text = 'some text1'
-  fig.update_layout(go.Layout(title='some text3',))
-  # print('file updated!')
-  # fig.update_layout(mapbox_style="carto-positron")
-  
-  
-  # fig.show()
-  fig_json = fig.to_json()
-  with open('plot.json', 'w') as fd:
-    fd.write(fig_json)
-  with open('./gmme-map/static/plot.json', 'w') as fd:
-    fd.write(fig_json)
-    print('plotly scatter map has been saved as "plot.json"')
-
-
-# PLOTLY FIGURE METHODS CONVENTIONS
-
-# fig.update_layout(title_text="update_layout() Syntax Example",
-#                   title_font=dict(size=30))
-
-
-# fig.update_layout(title=dict(text="update_layout() Syntax Example"),
-#                              font=dict(size=30))
-
-# USE EITHER THE FIRST OR LAST ONE BELOW:
-# fig.update_layout(title_text="update_layout() Syntax Example",
-#                   title_font_size=30)
-
-# fig.update_layout({"title": {"text": "update_layout() Syntax Example",
-#                              "font": {"size": 30}}})
-
-# fig.update_layout(title=go.layout.Title(text="update_layout() Syntax Example",
-#                                         font=go.layout.title.Font(size=30)))
-
-# Testing
-
-# plotMap()
